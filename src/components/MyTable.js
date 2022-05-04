88906e32cbc95fd4864d8cd4467566484771669e
DoreamonZED <2433255732@qq.com>
2022-05-04 23:19:26 +0800

    表格速查功能,表格数据格式

--- src/components/MyTable.js
+++ src/components/MyTable.js
@@ -1,25 +1,40 @@
 // import 
 
 export default function MyTable(){
-    return (<div>
-        123
-        {/* <table>
-            <thead>
-                <th>
-                    <td>123</td>
-                </th>
-                <th>
-                    <td>123</td>
-                </th>
-            </thead>
-            <tbody>
-                <th>
-                    <td>456</td>
-                </th>
-                <th>
-                    <td>456</td>
-                </th>
-            </tbody>
-        </table> */}
-    </div>)
+
+    let tables =[
+        {
+            title: "字体属性",
+            th: ["值","属性"],
+            tr: [
+                ["aligen-content", "规定弹性容器之间的对其方式"],
+                ["align-items", "规定弹性容器内项目的对齐方式"],
+                ["align-self", "规定弹性容器内所选项目的对齐方式"],
+            ]
+        }
+    ]
+
+    return <>{
+        tables.map(( tableItem, tableIndex) => <div key={tableIndex}>
+            <h4>{ tableItem.title }</h4>
+            <table>
+                <thead>
+                    <tr>
+                        {
+                            tableItem.th.map((trItem, trIndex) => <th key={trIndex}>{trItem}</th>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        tableItem.tr.map((trItem, trIndex) => <tr key={trIndex}>
+                            {
+                                trItem.map((tdItem, tdIndex) => <td key={tdIndex}>{ tdItem }</td>)
+                            }
+                        </tr>)
+                    }
+                </tbody>
+            </table>
+        </div>)
+    }</>
 }
